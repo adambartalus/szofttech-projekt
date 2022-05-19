3af119c0ffaed8fdf78689d09cda01e12d796cee
Bartalus Ádám <m7douv@inf.elte.hu>
2022-05-20 00:19:44 +0200

    test3

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,11 +1,10 @@
-
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class BasicTest {
     
     @Test
-    void testMethod() {
+    public void testMethod() {
         assertTrue(true);
     }
 }
