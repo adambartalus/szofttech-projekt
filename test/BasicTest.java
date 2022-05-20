88ffbfc53b7a9362a703dd5d311d70ffe8cea639
Kiss Mark Andras <gafyj1@inf.elte.hu>
2022-05-20 09:48:32 +0200

    test

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 import model.BasicTower;
+import model.HealSpell;
+import model.MeteorSpell;
 import model.Unit;
 
 import static org.junit.Assert.assertTrue;
@@ -25,6 +27,16 @@ public class BasicTest {
     testTower.turn(game);
     assertTrue(testunit.getHp() == 830);
     testTower.upgrade();
+    testTower.turn(game);
+    assertTrue(testunit.getHp() == 750);
+    MeteorSpell testspell1 = new MeteorSpell();
+    testspell1.Effect(new Position(2,3), game, game.GetPlayer(1));
+    assertTrue(testunit.getHp() == 250);
+    HealSpell testspell2 = new HealSpell();
+    testspell2.Effect(new Position(3,4), game, game.GetPlayer(0));
+    assertTrue(testunit.getHp() == 750);
+    FreezeSpell testspell3 = new FreezeSpell();
+    testspell3.Effect(new Position(4,4), game, game.GetPlayer(0));
     testTower.turn(game);
     assertTrue(testunit.getHp() == 750);
 }
