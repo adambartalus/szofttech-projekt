bf5298695bac246e8df9ad03164831a846e20026
Kiss Mark Andras <gafyj1@inf.elte.hu>
2022-05-20 09:27:24 +0200

    added tests

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+
+import model.Unit;
+
 import static org.junit.Assert.assertTrue;
 
 public class BasicTest {
@@ -7,4 +10,13 @@ public class BasicTest {
     public void testMethod() {
         assertTrue(true);
     }
+    
+    Dimension d = new Dimension (10,15);
+    Game game = new Game(d, "Player1", "Player2");
+    Unit testunit = new Unit(new Position(3,3),1,1000);
+    testunit.findpath(new Position(3,8));
+    testunit.step();
+    assertTrue(testunit.GetPosition().equals(new Position(3,4)));
+    testunit.takeDamage(100);
+    assertTrue(testunit.getHp() == 900);
 }
