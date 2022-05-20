bd17daf719196724fe54afafc1946722ae962c8e
Kiss Mark Andras <gafyj1@inf.elte.hu>
2022-05-20 09:35:18 +0200

    more test

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import model.BasicTower;
 import model.Unit;
 
 import static org.junit.Assert.assertTrue;
@@ -14,9 +15,16 @@ public class BasicTest {
     Dimension d = new Dimension (10,15);
     Game game = new Game(d, "Player1", "Player2");
     Unit testunit = new Unit(new Position(3,3),1,1000);
+    testunit.owner = game.GetPlayer(0);
     testunit.findpath(new Position(3,8));
     testunit.step();
     assertTrue(testunit.GetPosition().equals(new Position(3,4)));
     testunit.takeDamage(100);
     assertTrue(testunit.getHp() == 900);
+    BasicTower testTower = new BasicTower(new Position (4,4),game.GetPlayer(1));
+    testTower.turn(game);
+    assertTrue(testunit.getHp() == 830);
+    testTower.upgrade();
+    testTower.turn(game);
+    assertTrue(testunit.getHp() == 750);
 }
